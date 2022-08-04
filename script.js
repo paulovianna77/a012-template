f61efdc52dc931422d2847c1a55ddc84041e591c
paulovianna <paulo2001.vianna@gmail.com>
2022-08-04 18:02:53 -0300

    Exercício de prática guiada

---
+++ script.js
@@ -0,0 +1,12 @@
+const media = 8;
+
+
+if(media >= 5){
+    console.log("Aprovada!!")
+}else if(media >= 3){
+    console.log("Recuperação!!")}
+else if(media < 3){
+    console.log("Reprovada!")
+}else{
+    console.log("Dados inválidos")
+}
